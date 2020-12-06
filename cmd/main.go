1e41c28bb05873a9737e23b573500bd9c0f75a50
Timofei Makarov <macintosh1999@mail.ru>
2020-12-06 22:32:49 +0300

    some main

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-
+	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/pgxpool"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-
+	db, err := pgxpool.Conn(pgx.ConnPoolConfig{
+		ConnConfig: pgx.ConnConfig{
+			User:     "admin",
+			Password: "postgres",
+			Port:     5432,
+			Database: "forum",
+		},
+		MaxConnections: 50,
+	})
+	pgxpool
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module subd
 
 go 1.15
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v4 v4.10.0 // indirect
+	github.com/lib/pq v1.9.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c // indirect
+	golang.org/x/text v0.3.4 // indirect
+)
